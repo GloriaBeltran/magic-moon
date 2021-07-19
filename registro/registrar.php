a08616691302d320b3e833af5bea9871c18d1366
FatzDev <mangelvargasb@gmail.com>
2021-07-18 20:35:42 -0500

    Regitro arreglado

--- registro/registrar.php
+++ registro/registrar.php
@@ -2,14 +2,6 @@
 
 require_once "../lib/db.config.php";
 
-$db = new DB();
-$consulta = $db->obtenerProductosCarrito($_COOKIE["uid"]);
-$productos = mysqli_fetch_all($consulta);
-// foreach ($productos as $producto) {
-// 	print $producto[0];
-// }
-print_r($productos);
-
 if (isset($_POST["nombre"])) {
 	$nombre = $_POST["nombre"];
 	$email = $_POST["email"];
