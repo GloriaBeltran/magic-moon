809ef8c4fd5e56fc6f4a42f9a84201af88be05c5
FatzDev <mangelvargasb@gmail.com>
2021-07-19 09:16:00 -0500

    Parte del carrito creado

--- lib/db.config.php
+++ lib/db.config.php
@@ -6,6 +6,7 @@ class DB {
 		$this->user = "root";
 		$this->password = "";
 		$this->database = null;
+		$this->uid = null;
 	}
 	// Funcion para definir la base de datos antes de usar una sentencia SQL
 	private function setDatabase($database) {
@@ -17,32 +18,36 @@ class DB {
 		return mysqli_connect($this->server, $this->user, $this->password ,$this->database);
 	}
 	// Crea un carrito de compras para un usuario
-	private function crearCarrito($uid) {
+	private function crearCarrito() {
 		// Define la base de datos a usar
 		$this->setDatabase("dbmagicmooncarrito");
 		// Crea el registro que va a contener los productos del carrito
-		return mysqli_query($this->connect(), "CREATE TABLE $uid (producto VARCHAR(255) NOT NULL PRIMARY KEY)");
+		return mysqli_query($this->connect(), "CREATE TABLE $this->uid (id VARCHAR(255) NOT NULL PRIMARY KEY, nombre VARCHAR(255) NOT NULL)");
 	}
 	// Obtiene los productos que el usuario tiene en su carrito
-	public function obtenerProductosCarrito($uid) {
+	public function obtenerProductosCarrito() {
 		// Define la base de datos a usar
 		$this->setDatabase("dbmagicmooncarrito");
 		// Ejecuta la sentencia SELECT para obtener los productos
-		return mysqli_query($this->connect(), "SELECT * FROM $uid");
+		return mysqli_query($this->connect(), "SELECT * FROM $this->uid");
+	}
+	public function agregarProductoAlCarrito($id, $nombre) {
+		$this->setDatabase("dbmagicmooncarrito");
+		// return mysqli_query($this->connect(), "INSERT INTO ".$_COOKIE['uid']."(id, nombre) VALUES('$id', '$nombre')");
 	}
 	// Este metodo registra un usuario con los parametros que se le den
 	public function registrarUsuario($nombre, $email, $telefono, $password) {
 		// Tambien crea un UserID aleatorio para cada usuario, para mayor seguridad
-		$uid = session_create_id();
+		$this->uid = session_create_id();
 		// Crea el carrito de compras para el usuario creado
-		$this->crearCarrito($uid);
+		$this->crearCarrito();
 		// Almacena el UserID en una cookie por 30 días para acceder despues
-		setcookie("uid", $uid, time()+(86400*30));
+		setcookie("uid", $this->uid, time()+(86400*30));
 
 		// Define la base de datos a usar
 		$this->setDatabase("dbmagicmoon");
 		// Crea el registro del usuario
-		return mysqli_query($this->connect(), "INSERT INTO tblusuarios(uid, nombre, email, telefono, password) VALUES('$uid', '$nombre', '$email', '$telefono', '$password')");
+		return mysqli_query($this->connect(), "INSERT INTO tblusuarios(uid, nombre, email, telefono, password) VALUES('$this->uid', '$nombre', '$email', '$telefono', '$password')");
 	}
 	// Obtiene la contraseña del usuario con el email que se de
 	public function getPassword($email) {
--- login/ingresar.php
+++ login/ingresar.php
@@ -11,26 +11,27 @@ if (isset($email)) {
 	$server_password = mysqli_fetch_row($consulta)[0];
 	if (isset($server_password)) {
 		if ($password == $server_password) {
-			print ('<script>'.
-						'Swal.fire({'.
-							'icon: "success",'.
-							'title: "Login exitoso"'.
-						'});'.
-				   '</script>');
+			print('<script>' .
+				'Swal.fire({' .
+				'icon: "success",' .
+				'title: "Login exitoso"' .
+				'});' .
+				'</script>');
+			setcookie("uid", , time() + (86400 * 30));
 		} else {
-			print ('<script>'.
-						'Swal.fire({'.
-							'icon: "error",'.
-							'title: "La contraseña no es correcta"'.
-						'});'.
-				   '</script>');
+			print('<script>' .
+				'Swal.fire({' .
+				'icon: "error",' .
+				'title: "La contraseña no es correcta"' .
+				'});' .
+				'</script>');
 		}
 	} else {
-		print ('<script>'.
-					'Swal.fire({'.
-					'icon: "error",'.
-					'title: "El usuario con ese email no existe"'.
-				'});'.
-		   '</script>');
+		print('<script>' .
+			'Swal.fire({' .
+			'icon: "error",' .
+			'title: "El usuario con ese email no existe"' .
+			'});' .
+			'</script>');
 	}
 }
